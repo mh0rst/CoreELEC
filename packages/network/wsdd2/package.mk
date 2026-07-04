aaa77207d563e7f96ea43473cf8796f7ee43e5cd
Moritz Horstmann <dev@peterzweg.at>
2026-07-04 15:31:15 +0200

    Fix removed wsdd2 repository, use own

--- packages/network/wsdd2/package.mk
+++ packages/network/wsdd2/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="wsdd2"
 PKG_VERSION="1.8.7"
-PKG_SHA256="b0b6b31522f4a5e39d075b31d59d57af9a567f543e0b39b2fbdfec324d30310a"
+PKG_SHA256="79d18e25a788cc7dcb752aa6043a75ef7f62518d3f07e9d2a0ed661ab57b93c4"
 PKG_LICENSE="GPL-3.0-or-later"
-PKG_SITE="https://github.com/Netgear/wsdd2/"
-PKG_URL="https://github.com/Netgear/wsdd2/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/mh0rst/wsdd2/"
+PKG_URL="https://github.com/mh0rst/wsdd2/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="make:host gcc:host"
 PKG_LONGDESC="WSD/LLMNR Discovery/Name Service Daemon"
 PKG_BUILD_FLAGS="+size"
